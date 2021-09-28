929cc7035ec2e85cdfdef62977d5a033ad5ece9f
User <vovik78@list.ru>
2021-09-28 22:32:43 +0300

    init

---
+++ src/test/java/com/brunis/vue_jwt_springboot/VueJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.basis_intellect.smis_vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmisVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
